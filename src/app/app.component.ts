58044bcc093742e06799e88d6bc7f2d3f9601f8e
Giancarlo Reina <giancarloreina125@gmail.com>
2022-09-29 11:42:20 -0500

    avancePrueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+import persona from 'src/assets/json/MOCK_DATA.json';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projectPractice';
+
+  Persona: any = persona;
 }
